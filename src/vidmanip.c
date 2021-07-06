e1bc2c275d1f3ec7a503546be81249111357375c
DrSTAHP <drstahp903@gmail.com>
2021-07-06 19:51:24 +0200

    Added a flag system for convenience

--- src/vidmanip.c
+++ src/vidmanip.c
@@ -7,6 +7,7 @@ Discord Video Manipulator by DrSTAHP
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -15,6 +16,11 @@ Discord Video Manipulator by DrSTAHP
 #define MVHD_TIME_OFFSET 12 //12 bytes
 #define BYTE 8 //8 bits
 #define EIGHT_BITS 1 //1 byte
+
+#define MAX_ARGUMENTS 255 
+
+#define TRUE 1
+#define FALSE 0
 
 void GetOffsetData(FILE* pFile,unsigned long iOffset, void* pBuffer,size_t BufferSize)
 {
@@ -100,44 +106,138 @@ void AppendVideoDuration(char* pFilePath, int iDuration, size_t iMVHD)
     fclose(pFile);
 }
 
-
-int main()
+void PromptFilePath(char* pBuffer)
 {
-    char szFilePath[2096];
-    int iDuration = 1;
-
     printf("Enter the video's path: ");
 #ifdef _WIN32                           //Gonna fix those ugly preprocessor blocks later.
 	scanf_s("%s", szFilePath);
 #elif __linux__
-	scanf("%s",szFilePath);
-#endif
-    
-	printf("Enter the duration in seconds: ");
-#ifdef _WIN32
-	scanf_s("%i", &iDuration);
-#elif __linux__
-	scanf("%i", &iDuration);
+	scanf("%s",pBuffer);
 #endif
 
+}
+
+void PromptDuration(int* pBuffer)
+{
+    printf("Enter the video's duration: ");
+#ifdef _WIN32                           //Gonna fix those ugly preprocessor blocks later.
+	scanf_s("%s", szFilePath);
+#elif __linux__
+	scanf("%i",pBuffer);
+#endif
+
+}
+
+void PrintHelp()
+{
+    printf("Flags:\n\n-f describes the file path\n-d describes the duration\n--help is used to help you\n\n");
+}
+
+void ProcessTheVideo(char* pFilePath,int* pDuration)
+{
     printf("\nFinding the mvhd...(if this takes a long time, something is not right)\n");
-    size_t iMVHD = GetmvhdOffset(szFilePath);
+    size_t iMVHD = GetmvhdOffset(pFilePath);
 
     if(iMVHD <= 0)
     {
         printf("mvhd was not found. Is this .mp4 even valid? Quitting.");
-        return 0;
+        return;
     }
 
     printf("mvhd is at byte: %li\n",iMVHD);
 
     printf("\nChanging the duration...\n");
-    AppendVideoDuration(szFilePath, iDuration, iMVHD);
+    AppendVideoDuration(pFilePath, *pDuration, iMVHD);
 
     printf("\nDONE!\n");
+}
+
+void HandleArguments(int argc, char** argv)
+{
+    char szFileFlag[3] = "-f";
+    char szDurationFlag[3] = "-d";
+    char szHelp[7] = "--help";
+
+    unsigned short iFileFlagPos = 0;
+    unsigned short iDurationFlagPos = 0; 
+    
+    for(unsigned int i = 1; i < argc; i++)
+    {
+        if (!strcmp(argv[i],szFileFlag))
+        {
+            iFileFlagPos = i;
+        }
+        else if(!strcmp(argv[i],szDurationFlag))
+        {
+            iDurationFlagPos = i; 
+        }
+        else if(!strcmp(argv[i],szHelp))
+        {
+            PrintHelp();
+            return;
+        }
+        else if(i != iFileFlagPos + 1 && i != iDurationFlagPos + 1) 
+        {
+            printf("Invalid argument %s ! Consider using --help\n\n",argv[i]);
+            return;
+        }
+    }
+
+    char szFilePath[2096];
+    int iDuration = 1;
 
 #ifdef _WIN32
-	system("pause");
+    strcpy_s(szFilePath, argv[iFileFlagPos]);
+#elif __linux__
+    strcpy(szFilePath, argv[iFileFlagPos + 1]);
 #endif
+
+    iDuration = atoi(argv[iDurationFlagPos + 1]);
+    
+    if(!iFileFlagPos)
+    {
+        PromptFilePath(szFilePath);
+    }
+
+    if(!iDurationFlagPos)
+    {
+        PromptDuration(&iDuration);
+    }
+
+    ProcessTheVideo(szFilePath, &iDuration);
+    #ifdef _WIN32
+        system("pause");
+    #endif
+}
+
+int main(int argc, char **argv)
+{
+    if(argc < 2) //First argument is the file's name in C.
+    {
+        //Program prompts for everything.
+
+        char szFilePath[2096];
+        int iDuration = 1;
+
+        PromptFilePath(szFilePath);
+        PromptDuration(&iDuration);
+        
+        ProcessTheVideo(szFilePath,&iDuration);
+
+    #ifdef _WIN32
+        system("pause");
+    #endif
+    }
+
+    else if(argc >= MAX_ARGUMENTS)
+    {
+        printf("Exceeded the maximum argument count!\n");
+    }
+
+    else 
+    {
+        HandleArguments(argc,argv);
+    }
+
     return 0;
 }
