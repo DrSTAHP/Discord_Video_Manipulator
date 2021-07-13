5230bf28d4a059971ebcb720ee5bab34b8e79ff8
DrSTAHP <drstahp903@gmail.com>
2021-07-13 16:14:41 +0200

    Removed split_bytes(), htonl/s is better.

--- src/shared.h
+++ src/shared.h
@@ -3,11 +3,18 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#ifdef __WIN32
+#include <winsock2.h>
+#elif __linux__
+#include <arpa/inet.h>
+#endif
 
 #define BYTE 8 //8 bits
 #define EIGHT_BITS 1 //1 byte
 
+#define TRUE 1
+#define FALSE 0
+
 extern void GetOffsetData(FILE* pFile, unsigned long iOffset, void* pBuffer, size_t BufferSize);
-extern void split_bytes(int iNumber, uint8_t* pBuffer);
 
 #endif
